08d89af5302c2a9fb24fd44c4b7e5e88ea69c2fe
Julia Efimovskaya <julia.efimovskaya666@gmail.com>
2021-08-26 14:33:46 +0300

    added todo list

---
+++ 33/wp_todoList/src/index.js
@@ -0,0 +1,8 @@
+import "./styles/styles";
+import "./styles/fonts";
+
+import "./components/todoList";
+//import "@fortawesome/fontawesome-free/js/brands";
+//import "@fortawesome/fontawesome-free/js/fontawesome";
+//import "@fortawesome/fontawesome-free/js/solid";
+import "@fortawesome/fontawesome-free/js/all";
