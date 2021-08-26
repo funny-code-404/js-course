d6129d833de88043f333e8699558d967e104d671
Agentrapyntsel <shinkovich79@gmail.com>
2021-08-26 15:44:21 +0300

    [33] toDo and calendar

--- 33/shinkovich/src/index.js
+++ 33/shinkovich/src/index.js
@@ -1,29 +1,6 @@
+
+import "@fortawesome/fontawesome-free/js/fontawesome";
+import "@fortawesome/fontawesome-free/js/solid";
 import './styles/styles';
-import data from '@assets/data';
-import image from '@assets/octocat';
-import ListItem from './components/ListItem';
-
-
-import html from "./partials/inProgress.html";//!!!!!!!!!!!!!
-
-
-const ul = document.createElement('ul');
-
-data.users.forEach(({ name }) => {
-	const ListItemElement = new ListItem(ul, name, image);
-	ListItemElement.createListItem();
-
-});
-
-document.body.insertAdjacentElement('afterbegin', ul);
-
-
-
-
-
-
-//!!!!!!!!!!!!!!!!!!!!!!!!
-
-//добавление html блока inProgr
-let app = document.querySelector('#app');
-app.innerHTML = html;
+import inPro from "./components/inPro";
+import calendar from "./components/calendar";
