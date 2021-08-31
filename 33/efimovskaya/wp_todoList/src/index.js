46b8b2bda3a1742d374d07b1d451f5924c5c90e1
Julia Efimovskaya <julia.efimovskaya666@gmail.com>
2021-08-31 23:29:56 +0300

    added my project into my holder

---
+++ 33/efimovskaya/wp_todoList/src/index.js
@@ -0,0 +1,5 @@
+import "./styles/styles";
+import "./styles/fonts";
+
+import "./components/todoList";
+import "@fortawesome/fontawesome-free/js/all";
--- 33/wp_todoList/src/index.js
+++
@@ -1,8 +0,0 @@
-import "./styles/styles";
-import "./styles/fonts";
-
-import "./components/todoList";
-//import "@fortawesome/fontawesome-free/js/brands";
-//import "@fortawesome/fontawesome-free/js/fontawesome";
-//import "@fortawesome/fontawesome-free/js/solid";
-import "@fortawesome/fontawesome-free/js/all";
