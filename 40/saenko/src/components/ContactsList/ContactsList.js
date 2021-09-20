d04c1ecdd6d6d0baf71fa9d850c10f1916807d42
Anna Saenko <61340209+feelfaque@users.noreply.github.com>
2021-09-20 12:02:02 +0300

    [40] added contacts list

--- 40/saenko/src/components/ContactsList/ContactsList.js
+++ 40/saenko/src/components/ContactsList/ContactsList.js
@@ -1,5 +1,11 @@
-function ContactsList () {
-return ();
+function ContactsList (props) {
+return (
+    <ul className="contacts-list">
+        {props.contacts.map((contact) =>
+            <li key={contact.id}><h3>{contact.name}</h3><p>{contact.phone}</p></li>
+        )}
+    </ul>
+);
 }
 
 export default ContactsList;
