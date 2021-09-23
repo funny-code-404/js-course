b04f200a6f0a5877a7fc0d960c08bdcfe174578a
olegkrasilev <65223358+olegkrasilev@users.noreply.github.com>
2021-09-23 09:16:02 +0300

    React Context/use Reducer TodoList

---
+++ 42/Krasilev/components/Form.js
@@ -0,0 +1,54 @@
+import React, {useState, useContext, useReducer} from 'react'
+import TodoContext from '../context/todoContext';
+
+
+
+
+const Form = () => {
+  const [inputValue, setInputValue] = useState('')
+  const [buttonID, setButtonID] = useState('')
+  let todoList = useContext(TodoContext);
+
+  const todoReducer = (state, action) => {
+    if(action.type === 'NEW_TODO') {
+      return [...state, {title: inputValue}]
+    }
+    if(action.type === 'REMOVE_TODO') {
+      return state.filter(item => item.title !== buttonID)
+    }
+    return state;
+  }
+
+  const initialState = todoList;
+
+ const [todoState, dispatchTodo] = useReducer(todoReducer, initialState)
+
+
+  const getInputHandler = (e) => {
+    setInputValue(e.target.value)
+  }
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatchTodo({type: 'NEW_TODO', title: inputValue})
+  }
+  const deleteHandler = (e) => {
+    setButtonID(e.target.id)
+    dispatchTodo({type: 'REMOVE_TODO'})
+  }
+  return (
+    <>
+    <form>
+      <input value={inputValue} type="text" onChange={getInputHandler}/>
+      <button onClick={submitHandler} type="submit">ADD TODO</button>
+    </form>
+    <h1>
+      {todoState.map((item) => (
+        <li key={item.title}>{item.title} <button id={item.title} onClick={deleteHandler}>DELETE</button></li>
+      ))}
+    </h1>
+    </>
+  )
+}
+
+export default Form;
