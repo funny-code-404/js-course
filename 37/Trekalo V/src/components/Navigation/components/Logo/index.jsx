f3088c5ff4d331e3e8a66a35ecb6d4247fc83001
valeriknirek1@gmail.com <valeriknirek1@gmail.com>
2021-12-17 15:49:56 +0300

    37-Homerwork/ made page with React

---
+++ 37/Trekalo V/src/components/Navigation/components/Logo/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./styles.css";
+
+export function Logo(props) {
+  return (
+    <div>
+      <p className="logo">Your Logo</p>
+    </div>
+  );
+}
---
+++ 37/Trekalo V/src/components/Navigation/components/Nav/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./styles.css";
+
+export function Nav() {
+  return (
+    <div className="nav">
+      <div className="nav_item">About</div>
+      <div className="nav_item">Servicing</div>
+      <div className="nav_item">Portfodivo</div>
+      <div className="nav_item">Home</div>
+      <div className="nav_item">Contact us</div>
+      <div className="nav_item">Blog</div>
+    </div>
+  );
+}
