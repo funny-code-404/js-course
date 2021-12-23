40bed72147c8bc1c4a48339cdce70f485272b240
valeriknirek1@gmail.com <valeriknirek1@gmail.com>
2021-12-23 12:56:24 +0300

    created an app to show dogs

---
+++ 39/Trekalo V/src/components/index.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { useState } from "react";
+import "./style.css";
+
+const dogsArr = [
+  {
+    name: "Akita",
+    id: "akita",
+  },
+  {
+    name: "Chow",
+    id: "chow",
+  },
+  {
+    name: "Boxer",
+    id: "boxer",
+  },
+  {
+    name: "Husky",
+    id: "husky",
+  },
+  {
+    name: "Mix",
+    id: "mix",
+  },
+];
+
+export function Navigation() {
+  const [source, setSource] = useState("");
+
+  const addImage = async (dog) => {
+    const { message: image } = await fetch(
+      `https://dog.ceo/api/breed/${dog}/images/random`
+    ).then((data) => data.json());
+
+    setSource(image);
+  };
+
+  return (
+    <div>
+      <div className="wrapper">
+        {dogsArr.map((dog) => (
+          <div className="dogs" onClick={() => addImage(dog.id)}>
+            {dog.name}
+          </div>
+        ))}
+      </div>
+      {!source && <p className="title">Click and dog will load</p>}
+      {source && <img className="image" src={source} alt="" />}
+    </div>
+  );
+}
