8af663f631a4c97bd744a67bde08fc5a4455315e
Darya Kalenik <kalenikdarya@gmail.com>
2022-03-13 00:54:54 +0300

    Revert "[43] redux thunk"
    This reverts commit ca4747b94d81349c2a9bc4f7aaa03a221ec8f53c.

--- 43/daryakalenik/src/redux/initialState.js
+++
@@ -1,5 +0,0 @@
-import { initialDataState } from "../ducks/comments/reducer";
-
-const initialState = { data: initialDataState };
-
-export default initialState;
--- 43/daryakalenik/src/redux/rootReducer.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from "redux";
-import { commentsReducer } from "../ducks/comments/reducer";
-
-const rootReducer = combineReducers({ data: commentsReducer });
-
-export default rootReducer;
